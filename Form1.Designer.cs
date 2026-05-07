b1da2ad4a4aa24aeb7cdfbef01be0a46eef2a9f0
eugenio <eugenio@lantec2000.com>
2026-05-07 16:20:44 +0200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace PC7866
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            statusStrip1 = new StatusStrip();
+            menuStrip1 = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            modoToolStripMenuItem = new ToolStripMenuItem();
+            parámetrosToolStripMenuItem = new ToolStripMenuItem();
+            informesToolStripMenuItem = new ToolStripMenuItem();
+            acercaDeToolStripMenuItem = new ToolStripMenuItem();
+            abrirToolStripMenuItem = new ToolStripMenuItem();
+            guardarToolStripMenuItem = new ToolStripMenuItem();
+            guardarComoToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            mToolStripMenuItem = new ToolStripMenuItem();
+            automáticoToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Location = new Point(0, 428);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 0;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem, modoToolStripMenuItem, parámetrosToolStripMenuItem, informesToolStripMenuItem, acercaDeToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { abrirToolStripMenuItem, guardarToolStripMenuItem, guardarComoToolStripMenuItem, salirToolStripMenuItem });
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(60, 20);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // modoToolStripMenuItem
+            // 
+            modoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { mToolStripMenuItem, automáticoToolStripMenuItem });
+            modoToolStripMenuItem.Name = "modoToolStripMenuItem";
+            modoToolStripMenuItem.Size = new Size(51, 20);
+            modoToolStripMenuItem.Text = "Modo";
+            // 
+            // parámetrosToolStripMenuItem
+            // 
+            parámetrosToolStripMenuItem.Name = "parámetrosToolStripMenuItem";
+            parámetrosToolStripMenuItem.Size = new Size(79, 20);
+            parámetrosToolStripMenuItem.Text = "Parámetros";
+            // 
+            // informesToolStripMenuItem
+            // 
+            informesToolStripMenuItem.Name = "informesToolStripMenuItem";
+            informesToolStripMenuItem.Size = new Size(66, 20);
+            informesToolStripMenuItem.Text = "Informes";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            acercaDeToolStripMenuItem.Size = new Size(72, 20);
+            acercaDeToolStripMenuItem.Text = "Acerca De";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            abrirToolStripMenuItem.Size = new Size(180, 22);
+            abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            guardarToolStripMenuItem.Size = new Size(180, 22);
+            guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            guardarComoToolStripMenuItem.Size = new Size(180, 22);
+            guardarComoToolStripMenuItem.Text = "Guardar como";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(180, 22);
+            salirToolStripMenuItem.Text = "Salir";
+            // 
+            // mToolStripMenuItem
+            // 
+            mToolStripMenuItem.Name = "mToolStripMenuItem";
+            mToolStripMenuItem.Size = new Size(180, 22);
+            mToolStripMenuItem.Text = "Manual";
+            // 
+            // automáticoToolStripMenuItem
+            // 
+            automáticoToolStripMenuItem.Name = "automáticoToolStripMenuItem";
+            automáticoToolStripMenuItem.Size = new Size(180, 22);
+            automáticoToolStripMenuItem.Text = "Automático";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private StatusStrip statusStrip1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem abrirToolStripMenuItem;
+        private ToolStripMenuItem guardarToolStripMenuItem;
+        private ToolStripMenuItem guardarComoToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem modoToolStripMenuItem;
+        private ToolStripMenuItem parámetrosToolStripMenuItem;
+        private ToolStripMenuItem informesToolStripMenuItem;
+        private ToolStripMenuItem acercaDeToolStripMenuItem;
+        private ToolStripMenuItem mToolStripMenuItem;
+        private ToolStripMenuItem automáticoToolStripMenuItem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace PC7866
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
